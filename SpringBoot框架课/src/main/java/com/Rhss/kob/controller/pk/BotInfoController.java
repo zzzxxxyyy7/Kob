072d23bba8017b23b743217d352b0d66ae7c7dcc
Rhss <3567878896@qq.com>
2023-02-23 18:29:32 +0800

    项目初步部署

--- SpringBoot框架课/src/main/java/com/Rhss/kob/controller/pk/BotInfoController.java
+++ SpringBoot框架课/src/main/java/com/Rhss/kob/controller/pk/BotInfoController.java
@@ -14,10 +14,10 @@ public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
     public Map<String , String> getBotInfo(){
-        Map<String , String> map = new HashMap<>();
-        map.put("Name" , "tiger");
-        map.put("rating" , "1500");
-        return map;
+        Map<String , String> bot1 = new HashMap<>();
+        bot1.put("name" , "tiger");
+        bot1.put("rating" , "1500");
+        return bot1;
     }
 
 }
